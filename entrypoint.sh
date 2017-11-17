9b1b95e5de801964960ea77c0afd0f2f58c41229
fsword <li.jianye@gmail.com>
2017-11-17 18:11:42 +0800

    添加EMAIL参数，供https设定使用

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,15 @@ CONF_FILE="/etc/Caddyfile"
 if [ "$SITE" != "" ]
 then
   echo "$SITE {" > $CONF_FILE
-  sed '1d' /Caddyfile.template >> $CONF_FILE
+else
+  head -n 1 /Caddyfile.template > $CONF_FILE
 fi
+
+if [ "$TLS_EMAIL" != "" ]
+then
+  echo "  tls $TLS_EMAIL" >> $CONF_FILE
+fi
+
+sed '1d' /Caddyfile.template >> $CONF_FILE
 
 exec "$@"
