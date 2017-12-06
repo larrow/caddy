593bfe1f9a8e6289e12847bbc5f6e2de3029dba7
fsword <li.jianye@gmail.com>
2017-12-06 17:42:34 +0800

    允许直接绑定Caddyfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,23 @@
 
 set -e
 
-CONF_FILE="/etc/Caddyfile"
-
-if [ "$SITE" != "" ]
+if [ "$CONF_FILE" = "" ]
 then
-  echo "$SITE {" > $CONF_FILE
-else
-  head -n 1 /Caddyfile.template > $CONF_FILE
+  CONF_FILE="/etc/Caddyfile"
+
+  if [ "$SITE" != "" ]
+  then
+    echo "$SITE {" > $CONF_FILE
+  else
+    head -n 1 /Caddyfile.template > $CONF_FILE
+  fi
+
+  if [ "$TLS_EMAIL" != "" ]
+  then
+    echo "  tls $TLS_EMAIL" >> $CONF_FILE
+  fi
+
+  sed '1d' /Caddyfile.template >> $CONF_FILE
 fi
+exec "$@ --conf $CONF_FILE"
 
-if [ "$TLS_EMAIL" != "" ]
-then
-  echo "  tls $TLS_EMAIL" >> $CONF_FILE
-fi
-
-sed '1d' /Caddyfile.template >> $CONF_FILE
-
-exec "$@"
